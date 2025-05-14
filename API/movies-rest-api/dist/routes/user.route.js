edbacbe886ebf3eba171a73146a266ad396f0e04
Nguyễn Ngọc Huy <nguyenhuypm1@gmail.com>
2025-05-15 03:39:07 +0700

    update-api

---
+++ API/movies-rest-api/dist/routes/user.route.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const controllers_1 = require("../controllers");
+const middleware_1 = require("../middleware");
+const schema_1 = require("../schema");
+const router = (0, express_1.Router)();
+router.post("/signup", (0, middleware_1.validateRequestSchema)(schema_1.signupSchema), controllers_1.UserController.signUp);
+router.post("/login", (0, middleware_1.validateRequestSchema)(schema_1.loginSchema), controllers_1.UserController.login);
+router.put("/profile", middleware_1.authenticateToken, (0, middleware_1.validateRequestSchema)(schema_1.updateUserSchema), controllers_1.UserController.updateProfile);
+router.get("/profile", middleware_1.authenticateToken, controllers_1.UserController.getProfile);
+router.post("/forgot-password", [], controllers_1.UserController.forgotPassword);
+router.post("/verify-otp", [], controllers_1.UserController.verifyOtp);
+router.post("/change-password", middleware_1.authenticateToken, controllers_1.UserController.changePassword);
+exports.default = router;
---
+++ API/movies-rest-api/src/interfaces/user.interface.ts
@@ -0,0 +1,21 @@
+import {
+  CreationOptional,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from "sequelize";
+
+export interface User
+  extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  id: CreationOptional<number>;
+  name: string;
+  email: string;
+  birthday: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  password?: string;
+  photoURL?: string;
+  isAdmin?: boolean;
+  otp?: string | null;
+  otpExpiry?: Date | null;
+}
